a9b9d737d740af33faa0358e3cfc35114f4d6abb
VivianSun0927 <viviansun0927@gmail.com>
2023-04-03 20:48:30 -0700

    Updating working interactive map, slight changes in Mongo

--- logic.js
+++ logic.js
@@ -4,20 +4,32 @@ let myMap = L.map("map", {
     39.8282, -98.5795
   ],
   zoom: 5,
+  // Add will-change property to the map
+  willChange: 'transform'
 });
 
 // Add a tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18,
 }).addTo(myMap);
 
+// Add a 'load' event listener to the tile layer
+tileLayer.on('load', function() {
+  console.log('Tile layer loaded');
+});
+
+// ... Rest of the code ...
+
+// Add a 'load' event listener to the tile layer
 // Define the path to the CSV file and use D3 to fetch the data from the CSV
-d3.csv("/Resources/Mother Jones - Mass Shootings Database, 1982 - 2023 - Sheet1.csv", function(data) {
-  console.log(data);
+d3.csv("/Resources/Mother Jones - Mass Shootings Database, 1982 - 2023 - Sheet1.csv")
+  .then(function(data) {
+    console.log(data);
 
     // Create an empty feature group to hold the markers
     const markerGroup = L.featureGroup().addTo(myMap);
+    console.log(markerGroup);
 
     // Iterate through each row of the CSV data and add a marker for each row
     for (let i = 0; i < data.length; i++) {
@@ -32,18 +44,18 @@ d3.csv("/Resources/Mother Jones - Mass Shootings Database, 1982 - 2023 - Sheet1.
       const summary = row.summary;
 
       // Create a circle marker for the row
-      const marker = L.circleMarker([row.latitude, row.longitude], {
-        radius: fatalities * 5, // Scale the radius based on Number Killed
-        color: "red",
+      const marker = L.circleMarker([parseFloat(row.latitude), parseFloat(row.longitude)], {
+        radius: parseInt(fatalities) * 1.5,
+        color: "#FF5733",
         fillOpacity: 0.5
       }).addTo(markerGroup);
 
       // Create a popup for the marker
       const popupContent = `<b>Case:</b> ${caseName}<br><b>Location:</b> ${location}<br><b>Date:</b> ${date}<br><b>Fatalities:</b> ${fatalities}<br><b>Injured:</b> ${injured}<br><b>Summary:</b> ${summary}`;
       marker.bindPopup(popupContent);
+      console.log(markerGroup.getLayers());
     }
   })
   .catch(function(error) {
     console.log(error);
   });
-
